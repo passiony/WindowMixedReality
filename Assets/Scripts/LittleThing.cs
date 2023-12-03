46a9b657271dfb3a4ba7f5f01d74731e8d6ed077
passion <563129582@qq.com>
2023-12-03 21:18:25 +0800

    添加book和小物件

---
+++ Assets/Scripts/LittleThing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LittleThing : XRGrabInteractable
+{
+    private Vector3 originalScale;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        originalScale = transform.localScale;
+        transform.localScale *= 1.2f;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        transform.localScale = originalScale;
+    }
+}
+
+
